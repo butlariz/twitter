682694bb861d7559e1a84551adc1e86b2485f21b
butlariz <laris.santana09@gmail.com>
2018-07-06 14:07:35 -0300

    Versão 0.0.3 As cores trocam com a quantidade de caracteres e o cursor muda quando o botão é ativado ou desativado.

--- app.js
+++ app.js
@@ -2,24 +2,46 @@ var btn = document.querySelector('button');
 showCount = document.getElementsByClassName("char-count")[0];
  
 function countText(value){
+	// Contar a quantidade de caracteres
 	var restChar = 140 - value.length;
 	showCount.textContent = restChar + " caracteres restantes"
-	if (restChar === 140 || restChar < 0) {
-		btn.disabled = true;
-  } else {
+	// Desativar ou ativar o botão
+	if (restChar < 0 || restChar > 140){
+		btn.disabled = true;	
+	} else {
 		btn.disabled = false;
+	}
+	// Trocar a cor do contador
+	if (restChar > 20){
+		showCount.setAttribute("style", "color:#6ad2f3");
+	}
+	if (restChar < 20){
+		showCount.setAttribute("style", "color:#d8d500");
+	} 
+	if (restChar < 10){
+		showCount.setAttribute("style", "color:#f77009");
+	}
+	if (restChar < 0){
+		showCount.setAttribute("style", "color:#e81414");
 	}
 	return false;
 }
 
 function tweet() {
+	// Pegar o texto e mostrar na timeline
 	var newTweet = document.getElementById("inputTwitter").value;
 	var postTimeline = document.createElement('li');
 	postTimeline.textContent = newTweet; 
 	var timeline = document.getElementById('timeline');
 	timeline.prepend(postTimeline);
-	document.getElementById("formTwitter").reset();
-	btn.disabled = true;
-	showCount.textContent = "140 caracteres restantes"
+	resetTweet();
 	return false;
 } 
+
+function resetTweet(){
+	// Resetar formulário, desativar o botão, e resetar o contador
+	document.getElementById("formTwitter").reset();
+	btn.disabled = true;
+	showCount.textContent = "140 caracteres restantes";
+	return false;
+}
