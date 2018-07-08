09f031a9c04e58a24564678cb095f90939e4eb5a
butlariz <laris.santana09@gmail.com>
2018-07-08 15:17:30 -0300

    Versão 0.0.5 e 0.0.6 - O textarea acompanha o tamanho do texto. Aparece a hora em que o tweet foi postado

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// Variáveis e dados globais 
 var newTweet = document.getElementById("textTwitter");
-var btn = document.querySelector('button');
-showCount = document.getElementsByClassName("char-count")[0];
+var btn = document.querySelector('#btnTweet');
+btn.addEventListener("click", tweet);
+var showCount = document.getElementsByClassName("char-count")[0];
  
 function countText(value){
 	// Contar a quantidade de caracteres
@@ -28,19 +30,30 @@ function countText(value){
 	//Resize TextArea
 	if (newTweet.scrollHeight > 70) {
 		if (newTweet.offsetHeight < newTweet.scrollHeight){
-			newTweet.setAttribute("style", "height:" + newTweet.scrollHeight + "px");
+			olderRows = parseInt(newTweet.getAttribute("rows"));
+			newTweet.setAttribute("rows", olderRows + 1);
+			newTweet.setAttribute("style", "height:" + newTweet.scrollHeight +"px");
 		} 		
 		if (newTweet.offsetHeight > newTweet.scrollHeight){
-			newTweet.setAttribute("style", "height:" + (newTweet.scrollHeight - 15) + "px");
+			olderRows = parseInt(newTweet.getAttribute("rows"));
+			newTweet.setAttribute("rows", olderRows - 1);
+			newTweet.setAttribute("style", "height:" + newTweet.scrollHeight +"px");
 		}
+}
+	if (newTweet.value.length === 0) {
+		newTweet.setAttribute("rows", 2);
+		newTweet.setAttribute("style", "height: 70px");
 	}
 	return false;
 }
 
 function tweet() {
+	event.preventDefault();
 	// Pegar o texto e mostrar na timeline
 	var postTimeline = document.createElement('li');
-	postTimeline.textContent = newTweet.value; 
+	// Chamar perfil e hora
+	var infoTweet = [document.querySelector("#profile").innerHTML,showTime()];
+	postTimeline.innerHTML = "<span class='info-tweet'>" + infoTweet[0] + " • " + infoTweet[1] + "</span>" + newTweet.value; 
 	var timeline = document.getElementById('timeline');
 	timeline.prepend(postTimeline);
 	resetTweet();
@@ -50,7 +63,16 @@ function tweet() {
 function resetTweet(){
 	// Resetar formulário, desativar o botão, e resetar o contador
 	document.getElementById("formTwitter").reset();
+	newTweet.setAttribute("rows", 2);
 	btn.disabled = true;
 	showCount.textContent = "140 caracteres restantes";
 	return false;
+}
+
+function showTime() {
+	// Mostrar a hora do tweet
+	theHour = new Date().getHours();
+	theMinutes = new Date().getMinutes();
+	resultTime = "<span class='time'>" + theHour + ((theMinutes < 10) ? ":0" : ":") + theMinutes + "</span>";
+	return resultTime;
 }
